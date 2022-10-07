4c01c5c3fee6d40cc26a1c648eba7cb61aa864a9
ambiesoft <ambiesoft.trueff@gmail.com>
2022-10-08 08:09:41 +0900

    nazo

--- book/package_crate/restaurant/src/lib.rs
+++ book/package_crate/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -14,4 +14,13 @@ mod front_of_house {
     }
 }
 
-fn main() {}
+pub fn eat_at_restaurant() {
+    // Absolute path
+    // 絶対パス
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    // 相対パス
+    front_of_house::hosting::add_to_waitlist();
+}
+
