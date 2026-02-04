2e742d3228dc2137d24d153abe322b3aaa67193c
ambiesoft <ambiesoft.trueff@gmail.com>
2026-02-04 14:46:07 +0900

    added

---
+++ freecodecamp/028204.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut count = 0;
+
+    'outer: loop {
+        'inner1: loop {
+            if count >= 20 {
+                break 'inner1;
+            }
+            count += 2;
+        }
+
+        count += 5;
+
+        'inner2: loop {
+            if count >= 30 {
+                break 'outer;
+            }
+        }
+
+        continue 'outer;
+    }
+}
